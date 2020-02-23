c449b2bdd1724ee7b0b0ce71d195b6c4dadcf879
SGCMarkus <markusornik@gmail.com>
2020-02-23 17:12:23 +0100

    sm8150-common: build IPACM

--- sm8150.mk
+++ sm8150.mk
@@ -191,3 +191,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
+
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal
