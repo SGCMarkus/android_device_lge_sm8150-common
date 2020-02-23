82989796dbf0c1b01e9fa3ad8c638c93e8be2681
SGCMarkus <markusornik@gmail.com>
2020-02-23 17:07:40 +0100

    sm8150-common: build drm hal

--- sm8150.mk
+++ sm8150.mk
@@ -154,3 +154,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator@1.0.vendor \
     vendor.qti.hardware.display.mapper@1.0.vendor
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey
