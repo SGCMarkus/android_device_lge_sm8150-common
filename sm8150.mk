5e7ab2c12a0977aadcbdd2aa8b5523605d6df7b5
SGCMarkus <markusornik@gmail.com>
2020-02-23 16:56:59 +0100

    sm8150-common: build bluetooth packages

--- sm8150.mk
+++ sm8150.mk
@@ -102,7 +102,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl:64 \
@@ -113,3 +112,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    liba2dpoffload \
+    libbthost_if \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor
