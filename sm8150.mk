c4d05bef005c3b6140687c084e5ebe985d7fb8d1
SGCMarkus <markusornik@gmail.com>
2020-02-27 15:47:07 +0100

    sm8150-common: build thermal service

--- sm8150.mk
+++ sm8150.mk
@@ -310,3 +310,9 @@ PRODUCT_PACKAGES += \
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msmnile
