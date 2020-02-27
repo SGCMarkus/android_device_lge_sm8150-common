c28f3e0aef72b87a76f131fc764ccc45b0ac518f
SGCMarkus <markusornik@gmail.com>
2020-02-27 15:44:32 +0100

    sm8150-common: move flashlmdd sensor configs to flashlmdd and build aosp sensor service

--- sm8150.mk
+++ sm8150.mk
@@ -300,3 +300,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy
+
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
