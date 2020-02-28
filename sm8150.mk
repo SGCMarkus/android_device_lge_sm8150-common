ad9d1a5881c7c1739137f4c221be36132fdf72a2
SGCMarkus <markusornik@gmail.com>
2020-02-28 18:08:13 +0100

    sm8150-common: add telephony packages

--- sm8150.mk
+++ sm8150.mk
@@ -378,6 +378,19 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
