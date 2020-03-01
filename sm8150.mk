a091c0a5d4cd1965013dc4f24fdeb1083adbc62c
SGCMarkus <markusornik@gmail.com>
2020-03-01 16:08:21 +0100

    sm8150-common: build configstore

--- sm8150.mk
+++ sm8150.mk
@@ -142,6 +142,11 @@ PRODUCT_PACKAGES += \
     cneapiclient \
     com.quicinc.cne
 
+# Context Hub
+PRODUCT_PACKAGES += \
+    android.hardware.contexthub@1.0-impl.generic \
+    android.hardware.contexthub@1.0-service
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.3-service \
