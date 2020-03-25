41f79ebbae9d7ce0fb5be45a007fe478155fa9f9
SGCMarkus <markusornik@gmail.com>
2020-03-25 11:34:37 +0100

    sm8150-common: add livedisplay hal

--- sm8150.mk
+++ sm8150.mk
@@ -257,6 +257,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.lge_msmnile
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-lge_sm8150
+
 # Media
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
