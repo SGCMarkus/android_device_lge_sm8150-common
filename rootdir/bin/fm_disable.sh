9dda383c4adff72de77791e28600f7c46198750b
SGCMarkus <markusornik@gmail.com>
2020-07-23 11:39:27 +0200

    sm8150-common: add script that disables qcom fm radio on devices/models that dont support it

---
+++ rootdir/bin/fm_disable.sh
@@ -0,0 +1,9 @@
+#!/vendor/bin/sh
+# Copyright (c) 2019 Qualcomm Technologies, Inc.
+# All Rights Reserved.
+# Confidential and Proprietary - Qualcomm Technologies, Inc.
+#
+# Copyright (c) 2009-2011, 2015, 2017 The Linux Foundation. All rights reserved.
+#
+
+pm disable com.caf.fmradio
