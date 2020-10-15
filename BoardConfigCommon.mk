fa34ba78afc3f57fe41b8736bf13b5c80c14f796
SGCMarkus <markusornik@gmail.com>
2020-10-15 18:49:07 +0200

    sm8150-common: Update qcom sepolicy include

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -195,7 +195,7 @@ TARGET_RIL_VARIANT := caf
 VENDOR_SECURITY_PATCH := 2020-01-01
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
