9c611754c7753ec70ae78cce4726c33fe4d40542
SGCMarkus <markusornik@gmail.com>
2020-10-15 20:39:36 +0200

    sm8150-common: add source built fingerprint hal

--- sm8150.mk
+++ sm8150.mk
@@ -204,7 +204,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1-service.lge_sm8150
 
 # FM packages
 PRODUCT_PACKAGES += \
