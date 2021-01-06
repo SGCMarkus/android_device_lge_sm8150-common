3399b15c449ed6ce98e4d65d6d72e50e35eab0c9
SGCMarkus <markusornik@gmail.com>
2021-01-06 11:49:36 +0100

    sm8150-common: drop BluetoothQti package
    * doesnt exist anymore

--- sm8150.mk
+++ sm8150.mk
@@ -150,7 +150,6 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    BluetoothQti \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
