0e617538000c612d7ba71adb1d1183ef13ccd8fa
SGCMarkus <markusornik@gmail.com>
2021-05-06 12:32:01 +0200

    sm8150-common: move building of fingerprint service to device specific trees
    * mh2lm needs prebuilt service, so dont build it for all

--- sm8150.mk
+++ sm8150.mk
@@ -210,10 +210,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.lge_msmnile
-
 # FM packages
 PRODUCT_PACKAGES += \
 	libqcomfm_jni \
