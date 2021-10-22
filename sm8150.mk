6ac8ebdb3d99fd885a283b457a9d04cdae338c31
SGCMarkus <markusornik@gmail.com>
2021-10-22 18:22:56 +0200

    sm8150-common: remove GSI keys and updatable apex
    not supported

--- sm8150.mk
+++ sm8150.mk
@@ -26,12 +26,6 @@ PRODUCT_PLATFORM := msmnile
 PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH)/bootctrl
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
-# GSI Keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
