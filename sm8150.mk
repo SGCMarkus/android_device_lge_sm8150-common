9b93eb416904b455b1be24ff46b4c320578a10f8
SGCMarkus <markusornik@gmail.com>
2021-10-24 04:49:57 +0200

    sm8150-common: add system_ext props

--- sm8150.mk
+++ sm8150.mk
@@ -43,6 +43,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 -include frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk
 
